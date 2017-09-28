a2da897ae9038e089dfc46bea768c6ba6a9d401d
Javier Martín Moreno <javier.martin23@alu.uclm.es>
2017-09-28 10:37:37 +0200

    Update App.java

--- src/main/java/preuba/preuba/App.java
+++ src/main/java/preuba/preuba/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello d afdsaf adsfdsfs!" );
+        System.out.println( "Hello d afdas fsadfsfsadfsafsfss!" );
     }
 }
